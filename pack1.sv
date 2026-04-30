23e5dba0ae67cee8d71dc0b54c34eba5d071ad41
Nada44yehia <109657564+Nada44yehia@users.noreply.github.com>
2026-04-30 15:34:45 +0300

    commit

---
+++ pack1.sv
@@ -0,0 +1,16 @@
+package pack1;
+	import uvm_pkg::*;
+	`include "uvm_macros.svh"
+  `include "my_sequence_item.sv"
+  `include "my_sequence_write_only.sv"
+  `include "my_sequence_read_only.sv"
+  `include "my_sequence_write_read.sv"
+  `include "my_driver.sv"        
+	`include "my_monitor.sv"
+  `include "my_sequencer.sv"   
+	`include "my_agent.sv"
+	`include "my_scoreboard.sv"
+	`include "my_subscriber.sv"
+	`include "my_env.sv"
+	`include "my_test.sv"
+endpackage 
